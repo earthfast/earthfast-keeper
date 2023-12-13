2ccb9a915b98b07c3708180b4570505a6cc6b880
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2023-12-13 09:26:56 -0500

    Add readme and new autotask

--- README.md
+++ README.md
@@ -2,8 +2,22 @@
 
 This repo stores the code for reconciliations (billing + payments) and network data backups. At a high level the flow is
 
-1. Defender Autotask (cron) => reconciles billing + payouts. This uses Defender Relayer to pay for transactions. The autotasks are stored in the `autotasks/` folder
-2. Github cron => read network status + execute data dump => create github PR for data backup purposes. The scripts called by Github are stored in the `src/` folder
-3. Defender Sentitels => triggers on contract calls => send email notifications for monitoring purposes.
+1. OZ Defender Autotask (cron) => reconciles billing + payouts, advances epoch
+2. Github cron => read network status + execute data dump => create github PR for data backup purposes
 
 
+#### Autotasks
+Autotasks are run by OZ Defender on a cron. The files in the autotask/ folder are just copies.
+
+To run an autotask locally for testing/debugging:
+1. Set the values in the .env file for the Relayer
+2. `node autotasks/reconcile-*.js`
+
+#### .github workflows
+After the Autotasks run on OZ Defender, the data for the chain is dumped into .json files and stored in the data/ folder. The autotask running is a pre-requisite. The scripts called by Github are stored in the `src/` folder
+
+#### src/
+Contains the actual scripts to check the status of a chain as well as dump the data state to be stored in the data/ folder
+
+#### OZ Sentinels
+Sentinels are alerts for monitors created in OZ Defender but not stored in this repo. You can set triggers on contract calls eg createProject, enableNode and send emails on these triggers
---
+++ autotasks/reconcile-testnet-sepolia.js
@@ -0,0 +1,168 @@
+const REGISTRY_ADDRESS = "0x172CEb125F6C86B7920fD391407aca0B5F416648";
+const NODES_ADDRESS = "0xB92A2e528fe6dB4d21423E59111A3f69D7293244";
+const BILLING_ADDRESS = "0xF38561BAD32fF946Ff4563519cD7344f21296c4D";
+
+const REGISTRY_ABI = [
+  {
+    inputs: [
+      { internalType: "bytes32", name: "topologyNodeId", type: "bytes32" },
+    ],
+    name: "advanceEpoch",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    "inputs": [],
+    "name": "getNodes",
+    "outputs": [
+      {
+        "internalType": "contract ArmadaNodes",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+];
+
+const NODES_ABI = [
+  {
+    inputs: [
+      { internalType: "bytes32", name: "operatorIdOrZero", type: "bytes32" },
+      { internalType: "bool", name: "topology", type: "bool" },
+    ],
+    name: "getNodeCount",
+    outputs: [{ internalType: "uint256", name: "count", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "bytes32", name: "operatorIdOrZero", type: "bytes32" },
+      { internalType: "bool", name: "topology", type: "bool" },
+      { internalType: "uint256", name: "skip", type: "uint256" },
+      { internalType: "uint256", name: "size", type: "uint256" },
+    ],
+    name: "getNodes",
+    outputs: [
+      {
+        components: [
+          { internalType: "bytes32", name: "id", type: "bytes32" },
+          { internalType: "bytes32", name: "operatorId", type: "bytes32" },
+          { internalType: "string", name: "host", type: "string" },
+          { internalType: "string", name: "region", type: "string" },
+          { internalType: "bool", name: "topology", type: "bool" },
+          { internalType: "bool", name: "disabled", type: "bool" },
+          { internalType: "uint256[2]", name: "prices", type: "uint256[2]" },
+          {
+            internalType: "bytes32[2]",
+            name: "projectIds",
+            type: "bytes32[2]",
+          },
+        ],
+        internalType: "struct ArmadaNode[]",
+        name: "values",
+        type: "tuple[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+const BILLING_ABI = [
+  {
+    inputs: [
+      { internalType: "bytes32", name: "topologyNodeId", type: "bytes32" },
+      { internalType: "bytes32[]", name: "nodeIds", type: "bytes32[]" },
+      { internalType: "uint256[]", name: "uptimeBips", type: "uint256[]" },
+    ],
+    name: "processBilling",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "bytes32", name: "topologyNodeId", type: "bytes32" },
+      { internalType: "bytes32[]", name: "nodeIds", type: "bytes32[]" },
+    ],
+    name: "processRenewal",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    "inputs": [],
+    "name": "getBillingNodeIndex",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+];
+
+const { ethers } = require("ethers");
+const {
+  DefenderRelayProvider,
+  DefenderRelaySigner,
+} = require("defender-relay-client/lib/ethers");
+
+const HashZero = "0x0000000000000000000000000000000000000000000000000000000000000000";
+const NODE_ID = "0x0000000000000000000000000000000000000000000000000000000000000000";
+
+const stringify = (value) => JSON.stringify(value, null, 2);
+
+exports.handler = async function (credentials) {
+  const provider = new DefenderRelayProvider(credentials);
+  const signer = new DefenderRelaySigner(credentials, provider);
+
+  const registry = new ethers.Contract(REGISTRY_ADDRESS, REGISTRY_ABI, signer);
+  const nodes = new ethers.Contract(NODES_ADDRESS, NODES_ABI, signer);
+  const billing = new ethers.Contract(BILLING_ADDRESS, BILLING_ABI, signer);
+
+  const nodeCount = await nodes.getNodeCount(HashZero, false);
+  const nodeData = await nodes.getNodes(HashZero, false, 0, nodeCount);
+  const nodeIds = nodeData.map((n) => n.id);
+  const uptimeBips = nodeData.map(() => 10000);
+
+  const billingArgs = [NODE_ID, [...nodeIds], [...uptimeBips]];
+  console.log(`Execute ArmadaBilling.processBilling ${stringify(billingArgs)}`);
+  console.log("billing index", (await billing.getBillingNodeIndex()).toString());
+  console.log("node count", nodeCount.toString());
+  const billingTx = await billing.processBilling(...billingArgs);
+  console.log(billingTx.hash);
+  await billingTx.wait();
+
+  const renewalArgs = [NODE_ID, [...nodeIds]];
+  console.log(`Execute ArmadaBilling.processRenewal ${stringify(renewalArgs)}`);
+  const renewalTx = await billing.processRenewal(...renewalArgs);
+  console.log(renewalTx.hash);
+  await renewalTx.wait();
+
+  const advanceArgs = [NODE_ID];
+  console.log(`Execute ArmadaRegistry.advanceEpoch ${stringify(advanceArgs)}`);
+  const advanceTx = await registry.advanceEpoch(...advanceArgs);
+  console.log(advanceTx.hash);
+  await advanceTx.wait();
+};
+
+// Only when running locally
+if (require.main === module) {
+  require("dotenv").config();
+  const { RELAYER_API_KEY: apiKey, RELAYER_API_SECRET: apiSecret } = process.env;
+  exports
+    .handler({ apiKey, apiSecret })
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+}
