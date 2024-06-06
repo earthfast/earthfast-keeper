eeed41f21a72868b251fea31895e589e5af682dc
Cheran <cheran.v.senthil@gmail.com>
2024-06-06 17:19:46 +0530

    Uncomment this

--- autotasks/reconcile-testnet-sepolia-staging.js
+++ autotasks/reconcile-testnet-sepolia-staging.js
@@ -107,17 +107,17 @@ exports.handler = async function (credentials) {
   const nodeIds = nodeData.map((n) => n.id);
   const uptimeBips = nodeData.map(() => 10000);
 
-//   const billingArgs = [NODE_ID, [...nodeIds], [...uptimeBips]];
-//   console.log(`Execute ArmadaBilling.processBilling ${stringify(billingArgs)}`);
-//   const billingTx = await billing.processBilling(...billingArgs);
-//   console.log(billingTx.hash);
-//   await billingTx.wait();
+  const billingArgs = [NODE_ID, [...nodeIds], [...uptimeBips]];
+  console.log(`Execute ArmadaBilling.processBilling ${stringify(billingArgs)}`);
+  const billingTx = await billing.processBilling(...billingArgs);
+  console.log(billingTx.hash);
+  await billingTx.wait();
 
-//   const renewalArgs = [NODE_ID, [...nodeIds]];
-//   console.log(`Execute ArmadaBilling.processRenewal ${stringify(renewalArgs)}`);
-//   const renewalTx = await billing.processRenewal(...renewalArgs);
-//   console.log(renewalTx.hash);
-//   await renewalTx.wait();
+  const renewalArgs = [NODE_ID, [...nodeIds]];
+  console.log(`Execute ArmadaBilling.processRenewal ${stringify(renewalArgs)}`);
+  const renewalTx = await billing.processRenewal(...renewalArgs);
+  console.log(renewalTx.hash);
+  await renewalTx.wait();
 
   const advanceArgs = [NODE_ID];
   console.log(`Execute ArmadaRegistry.advanceEpoch ${stringify(advanceArgs)}`);
