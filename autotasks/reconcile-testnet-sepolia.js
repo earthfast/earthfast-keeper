6e49cc9786ce44f643d432005ba86bf3d0562360
Mike <mahathaway93@gmail.com>
2024-11-20 15:19:16 -0500

    update autotasks for local execution by actions

--- autotasks/reconcile-testnet-sepolia.js
+++ autotasks/reconcile-testnet-sepolia.js
@@ -3,3 +3,16 @@ const reconcile = require('./reconcile');
 exports.handler = async function(credentials) {
   return reconcile.handler(credentials, './config/testnet-sepolia.js');
 };
+
+// Only when running locally
+if (require.main === module) {
+  require("dotenv").config();
+  const { RELAYER_API_KEY: apiKey, RELAYER_API_SECRET: apiSecret } = process.env;
+  exports
+    .handler({ apiKey, apiSecret })
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+}
